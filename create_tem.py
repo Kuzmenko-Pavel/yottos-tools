2d32321f442120b5a6b96a9a3a05cb914353e5e4
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2019-02-11 18:06:19 +0200

    up

--- create_tem.py
+++ create_tem.py
@@ -8,6 +8,7 @@ sys.setdefaultencoding('UTF8')
 import os
 import io
 from pymongo import MongoClient
+from itertools import izip_longest, count
 
 
 project_dir = os.path.dirname(os.path.realpath(__file__))
@@ -22,23 +23,42 @@ db = conn.getmyad_db
 
 class Tree(object):
     "Generic tree node."
-    def __init__(self, name='root', parent=None, level=None, children=None):
+    seq_id = count().next
+
+    def __init__(self, name='root', level=None):
+        self.id = self.seq_id()
         self.name = name
-        self.parent = parent
+        self.parent = None
+        self.path_id = 0
+        self.path_ids = []
         self.level = -1
         if level is not None:
             self.level = level
         self.children = []
-        if children is not None:
-            for child in children:
-                self.add_child(child)
 
     def __repr__(self):
         return self.name
 
     def add_child(self, node):
         assert isinstance(node, Tree)
+        node.parent = self
         self.children.append(node)
+
+    def create_id(self):
+        c = 0
+        for child in self.children:
+            c += 1
+            child.path_id = c
+            child.path_ids = self.path_ids[::]
+            child.path_ids.append(str(child.path_id))
+            child.create_id()
+
+    @property
+    def path(self):
+        if self.path_ids:
+            return '.'.join(self.path_ids)
+        else:
+            return '0'
 
 
 tree = Tree()
@@ -49,21 +69,21 @@ with io.open('tematic_cat.txt', mode="r", encoding="utf-8") as file:
         level = line.count('    ')
         if prev.level < level:
             parent = prev
-            node = Tree(name=line, parent=parent, level=level)
+            node = Tree(name=line.strip(), level=level)
             parent.add_child(node)
             prev = node
         elif prev.level == level:
             parent = prev.parent
-            node = Tree(name=line, parent=parent, level=level)
+            node = Tree(name=line.strip(), level=level)
             parent.add_child(node)
             prev = node
         elif prev.level > level:
             step = prev.level - level
             parent = prev
-            for x in range(0, step):
+            for x in range(0, step + 1):
                 parent = parent.parent
 
-            node = Tree(name=line, parent=parent, level=level)
+            node = Tree(name=line.strip(), level=level)
             parent.add_child(node)
             prev = node
 
@@ -71,8 +91,46 @@ with io.open('tematic_cat.txt', mode="r", encoding="utf-8") as file:
 def print_n(t):
     if t is None:
         return
-    print('%s%s' % ('\t' * t.level, t.name))
+    s = '%s %s %s %s' % (('\t' * t.level), str(t.id), t.name, t.path)
+    print(s)
+    doc = {
+        'id': t.id,
+        'path_id': t.path_id,
+        'name': t.name,
+        'path': t.path,
+        'children_id': [x.id for x in t.children]
+    }
+    if t.parent:
+        doc['parent_id'] = t.parent.id
+    db['thematic'].insert_one(doc)
     for child in t.children:
         print_n(child)
 
+tree.create_id()
 print_n(tree)
+
+
+# def cat_to_int_range(cat):
+#     max_size = 1000
+#     cats = [int(x) for x in cat.split('.')]
+#     cats = [i if i else j for i, j in izip_longest(cats, [0, 0, 0, 0, 0], fillvalue=0)]
+#     start_cats = cats[::]
+#     end_cats = cats[::]
+#     if cats[-1] < 1:
+#         l = len(cats)
+#         for x, y in enumerate(reversed(cats)):
+#             if y == 0:
+#                 start_cats[l-(x+1)] = 0
+#                 end_cats[l - (x + 1)] = max_size
+#             else:
+#                 break
+#     start = sum([y * (max_size ** x) for x, y in enumerate(reversed(start_cats))])
+#     end = sum([y * (max_size ** x) for x, y in enumerate(reversed(end_cats))])
+#     return start, end
+#
+#
+# print cat_to_int_range('10.0.0.12')
+# print cat_to_int_range('10.0.1')
+# print cat_to_int_range('10.0.0.12')
+# print cat_to_int_range('12')
+# print cat_to_int_range('77.88.21.8')
