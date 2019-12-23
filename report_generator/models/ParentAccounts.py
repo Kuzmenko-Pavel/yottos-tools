9d9d79bdcdca8296d5753e38c4aa315fcb34a090
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2019-12-23 12:07:37 +0200

    up

--- report_generator/models/ParentAccounts.py
+++ report_generator/models/ParentAccounts.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, unicode_literals
 __author__ = 'kuzmenko-pavel'
 from sqlalchemy import (Column, Integer, String, text)
 from sqlalchemy_utils import (ChoiceType, force_auto_coercion, force_instant_defaults)
-
+from sqlalchemy.orm import relationship
 
 from .meta import ParentBase
 from .__mixins__ import *
@@ -25,3 +25,11 @@ class ParentAccount(PrimaryKey, GUID, ParentBase, StatisticAccountRelation):
                           server_default=text("'" + str(AccountType.Customer.value) + "'::integer"))
 
     project = Column(ChoiceType(ProjectType, impl=Integer()))
+
+    blocks = relationship("ParentBlock", uselist=True, foreign_keys='ParentBlock.id_account',
+                          primaryjoin='ParentAccount.id == ParentBlock.id_account')
+
+    sites = relationship("ParentSite", uselist=True, foreign_keys='ParentSite.id_account',
+                         primaryjoin='ParentAccount.id == ParentSite.id_account')
+    campaigns = relationship("ParentCampaign", uselist=True, foreign_keys='ParentCampaign.id_account',
+                             primaryjoin='ParentAccount.id == ParentCampaign.id_account')
